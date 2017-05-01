58da7951535893fef08c428b82b99c659b3bf587
noemp96 <noemp96@gmail.com>
2017-05-01 12:14:45 +0200

    Update credencialesFlickr.js

--- credencialesFlickr.js
+++ credencialesFlickr.js
@@ -1,1 +1,8 @@
+var user_id = "Chipirosky";
+var api_key = "Sistem4sDistribuid0s";
 
+var user_id2 = "n_m_p_196";
+var api_key2 = "hola1234";
+
+var user_id3 = "Miky012";
+var api_key3 = "hola1234";
